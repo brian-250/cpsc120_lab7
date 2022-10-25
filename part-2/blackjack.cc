2e227130890a546b9837ffc320bf95d9e188a171
Brian Milian <brianmilian@csu.fullerton.edu>
2022-10-25 12:40:21 -0700

    added an exception to IsNumberCard function, created a variable in CardPoints function to return each card's points, shortened code by deleting redundant code

--- part-2/blackjack.cc
+++ part-2/blackjack.cc
@@ -14,32 +14,28 @@
 #include <string>
 #include <vector>
 
-bool IsAce(const std::string& card_name) {
-  if (card_name == "A") {
-    return true;
-  }
-  return false;
-}
+bool IsAce(const std::string& card_name) { return (card_name == "A"); }
 
 bool IsFaceCard(const std::string& card_name) {
-  if ((card_name == "J") || (card_name == "Q") || (card_name == "K")) {
-    return true;
-  }
-  return false;
+  return (card_name == "J") || (card_name == "Q") || (card_name == "K");
 }
 
 bool IsNumberCard(const std::string& card_name) {
-  if ((std::stoi(card_name) < 11) && (std::stoi(card_name) > 1)) {
-    return true;
+  int card_value{0};
+  bool is_number_card{true};
+  try {
+    card_value = stoi(card_name);
+    if ((card_value < 2) || (card_value > 10)) {
+      is_number_card = false;
+    }
+  } catch (std::exception const& e) {
+    is_number_card = false;
   }
-  return false;
+  return is_number_card;
 }
 
 bool IsCardName(const std::string& str) {
-  if ((IsAce(str)) || (IsFaceCard(str)) || (IsNumberCard(str))) {
-    return true;
-  }
-  return false;
+  return (IsAce(str)) || (IsFaceCard(str)) || (IsNumberCard(str));
 }
 
 bool AllArgumentsValid(const std::vector<std::string>& arguments) {
@@ -53,15 +49,16 @@ bool AllArgumentsValid(const std::vector<std::string>& arguments) {
 }
 
 int CardPoints(const std::string& card_name) {
+  int return_card_value{0};
   if (IsAce(card_name)) {
-    return 1;
+    return_card_value = 1;
   } else if (IsFaceCard(card_name)) {
-    return 10;
-  } else {
-    return std::stoi(card_name);
-  } 
+    return_card_value = 10;
+  } else if (IsNumberCard(card_name)) {
+    return_card_value = std::stoi(card_name);
+  }
+  return return_card_value;
 }
-
 bool HandContainsAce(const std::vector<std::string>& arguments) {
   for (int i = 1; i < arguments.size(); ++i) {
     if (IsAce(arguments.at(i))) {
@@ -71,19 +68,14 @@ bool HandContainsAce(const std::vector<std::string>& arguments) {
   return false;
 }
 
-bool IsBust(int score) {
-  if (score > 21) {
-    return true;
-  }
-  return false;
-}
- 
+bool IsBust(int score) { return score > 21; }
+
 int HandScore(const std::vector<std::string>& arguments) {
   int sum{0};
   for (int i = 1; i < arguments.size(); ++i) {
     sum += CardPoints((arguments.at(i)));
   }
-  if (!(IsBust(sum+10)) && (HandContainsAce(arguments))) {
+  if (!(IsBust(sum + 10)) && (HandContainsAce(arguments))) {
     sum += 10;
   }
   return sum;
